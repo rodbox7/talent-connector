b964b27548d0c371e07cc348fe0990de805067b2
rodbox7 <tarboxjohnd@gmail.com>
2025-10-10 09:57:53 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Safety valves so ESLint/TS can't block a production build
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true }
 };
