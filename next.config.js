1556b3e722128eff6cf6a32c5ab66dedca0955d0
rodbox7 <tarboxjohnd@gmail.com>
2025-10-10 09:31:32 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  pageExtensions: ['js', 'jsx'], // build only JS/JSX pages
-};
-module.exports = nextConfig;
+  reactStrictMode: true,
 
+  // Safety valves: if ESLint/TS try to block the build, ignore them.
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
+};
+
+module.exports = nextConfig;
