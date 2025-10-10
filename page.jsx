8be16aefb4e5794e2dc0c1b1d418f10730738e82
rodbox7 <tarboxjohnd@gmail.com>
2025-10-10 09:59:54 -0400

    Create page.jsx

---
+++ page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'grid', placeItems: 'center' }}>
+      <div>Hello from a clean Next.js app</div>
+    </div>
+  );
+}
