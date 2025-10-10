a943dd46c65d37630f591254b167cf65424e9a5f
rodbox7 <tarboxjohnd@gmail.com>
2025-10-10 09:53:08 -0400

    Delete app/page.jsx

--- app/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ minHeight: '100vh', display: 'grid', placeItems: 'center' }}>
-      <div>Hello from a clean Next.js app</div>
-    </div>
-  );
-}
