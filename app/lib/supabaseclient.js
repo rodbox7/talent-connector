e9d711fec828c59606cb47259d1eadc00bec32b8
rodbox7 <tarboxjohnd@gmail.com>
2025-10-10 09:52:11 -0400

    Delete app/lib/supabaseclient.js

--- app/lib/supabaseclient.js
+++
@@ -1,8 +0,0 @@
-// lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
-
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
-
-export const supabase = createClient(url, anon);
-export default supabase;
