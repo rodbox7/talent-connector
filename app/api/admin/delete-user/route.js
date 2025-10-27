d62a09d956da1ef20269ebac2fb7586d881f5f7b
rodbox7 <tarboxjohnd@gmail.com>
2025-10-27 18:06:45 -0400

    Update route.js

--- app/api/admin/delete-user/route.js
+++ app/api/admin/delete-user/route.js
@@ -2,6 +2,7 @@
 import { NextResponse } from 'next/server';
 import { supabaseAdmin } from '../../../../lib/supabaseAdmin';
 
+
 export async function POST(req) {
   try {
     const { id } = (await req.json()) || {};
